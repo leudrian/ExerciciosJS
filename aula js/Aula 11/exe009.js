dde4d5fca50555ff2dca8dfc07feeab424ffce8c
leudrian <56943865+leudrian@users.noreply.github.com>
2021-09-07 12:02:24 -0300

    Add files via upload

---
+++ aula js/Aula 11/exe009.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`O seu carro está a ${vel} Km/H`)
+if(vel>60){//Condição simples
+    console.log("Você ultrapassou a velocidade limite MULTADO!")
+}
+console.log("Diraja sempre com cinto de segurança")
